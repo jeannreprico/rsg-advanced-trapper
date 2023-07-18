045ab8f5b91970f2e1bc9a27da2176f1edcee404
Zepherlah <104341554+Zepherlah@users.noreply.github.com>
2023-07-19 04:43:37 +1000

    Add files via upload

---
+++ Install/npc.lua
@@ -0,0 +1,62 @@
+-- trappers
+   
+    {    -- trapper vendor Valentine 
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(-333.9737, 773.49157, 116.22194, 111.8759),
+    },
+    {    -- trapper vendor Saint Denis 
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(2832.3193, -1223.699, 47.654289, 190.36814),
+    },
+    {    -- trapper vendor Riggs Station 
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(-1007.607, -549.5084, 99.39138, 282.4226),
+    },
+    {    -- trapper vendor West Elizabeth 
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(-2844.197, 142.13876, 184.61907, 255.25524),
+    },
+    {    -- trapper vendor Stawberry 
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(-1745.992, -388.9831, 156.59568, 107.79673),
+    },
+    {    -- trapper vendor Tumbleweed 
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(-5511.721, -2951.048, -1.83548, 165.87483),
+    },
+    {    -- trapper vendor Emerald ranch
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(1421.0073, 381.1686, 90.3308, 155.2438),
+    },
+    {    -- trapper vendor Elysian pool
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(2541.4229, 809.8848, 75.9808, 90.8583),
+    },
+    {    -- trapper vendor grizzlies east north
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(1589.8363, 2193.2180, 324.2073, 294.5635),
+    },
+    {    -- trapper vendor Mount Hagen
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(-1632.8486, 1235.2946, 351.8740, 137.9310),
+    },
+    {    -- trapper vendor Heartland oil fields
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(497.0804, 579.8102, 110.1850, 0.2271),
+    },
+    {    -- trapper vendor The Heartlands
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(-127.6516, -24.2402, 96.0852, 105.7619),
+    },
+    {    -- trapper vendor Manzanita post
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(-1981.2384, -1650.3801, 117.0923, 128.7630),
+    },
+    {    -- trapper vendor Bolber Blade
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(1877.8077, -1854.6241, 42.7537, 105.7725),
+    },
+    {    -- trapper vendor Rio Bravo
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(-4621.4556, -3366.7170, 21.9793, 225.9604),
+    },
